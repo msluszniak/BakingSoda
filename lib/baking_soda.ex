0e94ef007ad1bfb7933232a2cdd1b4164eaca572
Mateusz <msluszniak@student.agh.edu.pl>
2022-02-20 17:34:31 +0100

    added new functionalities and new tests

--- lib/baking_soda.ex
+++ lib/baking_soda.ex
@@ -7,7 +7,7 @@ defmodule BakingSoda do
     case binary do
       # TODO: What are those first three bytes?
       <<128, 4, rest::binary>> ->
-        load(rest, [])
+        load(rest, [], [])
 
       _ ->
         {:error, "protocol version not supported"}
@@ -16,60 +16,311 @@ defmodule BakingSoda do
 
   # 46
   @stop ?.
+
   # 75
   @binint1 ?K
+
+  # 133
+  @tuple1 0x85
+
   # 134
   @tuple2 0x86
+
   # 135
   @tuple3 0x87
+
   # 140
   @short_binunicode 0x8C
+
   # 148
   @memoize 0x94
+
   # 149
   @framing 0x95
+
   # 71
   @binfloat ?G
+
   # 77
   @two_byte_int ?M
-  # @empty_list 0x5D #93
 
-  defp load(<<@binint1, int, rest::binary>>, stack) do
-    load(rest, [int | stack])
+  # 147
+  # (Push a global object (module.attr) on the stack)
+  @stack_global 0x93
+
+  # 40
+  # (Push markobject onto the stack)
+  @mark ?(
+
+  # 67
+  # There are two arguments:  the first is a 1-byte unsigned int giving
+  # the number of bytes, and the second is that many bytes, which are taken
+  # literally as the string content.
+  @short_binbytes ?C
+
+  # 137
+  # new false
+  @new_false 0x89
+
+  # 136
+  # new true
+  @new_true 0x88
+
+  # 78
+  # new None
+  @new_none ?N
+
+  # 74
+  # four byte signed integer
+  @four_byte_int ?J
+
+  # 116
+  # All the stack entries following the topmost markobject are placed into
+  # a single Python tuple, which single tuple object replaces all of the
+  # stack from the topmost markobject onward.  For example,
+  # Stack before: ... markobject 1 2 3 'abc'
+  # Stack after:  ... (1, 2, 3, 'abc')
+  @tuple ?t
+
+  # 82
+  @reduce ?R
+
+  # 104
+  # Read an object from the memo and push it on the stack.
+  # The index of the memo object to push is given by the 1-byte unsigned
+  # integer following.
+  @bin_get ?h
+
+  # 98
+  # Finish building an object, via __setstate__ or dict update.
+  #     Stack before: ... anyobject argument
+  #     Stack after:  ... anyobject
+  #     where anyobject may have been mutated, as follows:
+  #     If the object has a __setstate__ method,
+  #         anyobject.__setstate__(argument)
+  #     is called.
+  #     Else the argument must be a dict, the object must have a __dict__, and
+  #     the object is updated via
+  #         anyobject.__dict__.update(argument)
+  @build ?b
+
+  # 41
+  # Push an empty tuple
+  @empty_tuple ?)
+
+  # 93
+  # Push an empty list
+  @empty_list ?]
+
+  # 101
+  # append everyithing to list before :mark
+  @appends ?e
+
+  # 97
+  # append one element
+  @append ?a
+
+  # 129
+  # build a new object and push on a stack
+  @new_object 0x81
+
+  # 125
+  # push an empty dict on a stack
+  @empty_dict ?}
+
+
+  # 143
+  # push an empty set on a stack
+  @empty_set 0x8F
+
+  # 66
+  # Push a Python bytes object.
+  # There are two arguments:  the first is a 4-byte little-endian unsigned int
+  # giving the number of bytes, and the second is that many bytes, which are
+  # taken literally as the bytes content.
+  @binbytes ?B
+
+  # 117
+  # Add an arbitrary number of key+value pairs to an existing dict.
+  # The slice of the stack following the topmost markobject is taken as
+  # an alternating sequence of keys and values, added to the dict
+  # immediately under the topmost markobject.  Everything at and after the
+  # topmost markobject is popped, leaving the mutated dict at the top
+  # of the stack.
+  # Stack before:  ... pydict markobject key_1 value_1 ... key_n value_n
+  # Stack after:   ... pydict
+  # where pydict has been modified via pydict[key_i] = value_i for i in
+  # 1, 2, ..., n, and in that order.
+  @set_items ?u
+
+  # 106
+  # Read an object from the memo and push it on the stack.
+  # The index of the memo object to push is given by the 4-byte unsigned
+  # little-endian integer following.
+  @long_bin_get ?j
+
+  # 115
+  # Add a key+value pair to an existing dict.
+  # Stack before:  ... pydict key value
+  # Stack after:   ... pydict
+  # where pydict has been modified via pydict[key] = value.
+  @set_item ?s
+
+  defp load(<<@binint1, int, rest::binary>>, stack, memo) do
+    load(rest, [int | stack], memo)
   end
 
-  defp load(<<@short_binunicode, size::little, binary::size(size)-binary, rest::binary>>, stack) do
-    load(rest, [binary | stack])
+  defp load(
+         <<@short_binunicode, size::little, binary::size(size)-binary, rest::binary>>,
+         stack,
+         memo
+       ) do
+    load(rest, [binary | stack], memo)
   end
 
-  defp load(<<@tuple2, rest::binary>>, [two, one | stack]) do
-    load(rest, [{one, two} | stack])
+  defp load(
+         <<@short_binbytes, size::little, binary::size(size)-binary, rest::binary>>,
+         stack,
+         memo
+       ) do
+    load(rest, [binary | stack], memo)
   end
 
-  defp load(<<@tuple3, rest::binary>>, [three, two, one | stack]) do
-    load(rest, [{one, two, three} | stack])
+  defp load(<<@tuple1, rest::binary>>, [one | stack], memo) do
+    load(rest, [{one} | stack], memo)
   end
 
-  defp load(<<@binfloat, value::float-big, rest::binary>>, stack) do
-    load(rest, [value | stack])
+  defp load(<<@tuple2, rest::binary>>, [two, one | stack], memo) do
+    load(rest, [{one, two} | stack], memo)
   end
 
-  defp load(<<@two_byte_int,second,first,rest::binary>>, stack) do
+  defp load(<<@tuple3, rest::binary>>, [three, two, one | stack], memo) do
+    load(rest, [{one, two, three} | stack], memo)
+  end
+
+  defp load(<<@binfloat, value::float-big, rest::binary>>, stack, memo) do
+    load(rest, [value | stack], memo)
+  end
+
+  defp load(<<@four_byte_int, value::32-integer-big-signed, rest::binary>>, stack, memo) do
+    load(rest, [value | stack], memo)
+  end
+
+  defp load(<<@two_byte_int, second, first, rest::binary>>, stack, memo) do
     <<num::integer-size(2)-unit(8)>> = <<first, second>>
-    load(rest, [num | stack])
+    load(rest, [num | stack], memo)
   end
 
-  # TODO: handle memoization
-  defp load(<<@memoize, rest::binary>>, stack) do
-    load(rest, stack)
+  defp load(<<@memoize, rest::binary>>, [top | _] = stack, memo) do
+    load(rest, stack, [top | memo])
+  end
+
+  defp load(<<@mark, rest::binary>>, stack, memo) do
+    load(rest, [:mark | stack], memo)
   end
 
   # TODO: handle framing
-  defp load(<<@framing, _size::64, rest::binary>>, stack) do
-    load(rest, stack)
+  defp load(<<@framing, _size::64, rest::binary>>, stack, memo) do
+    load(rest, stack, memo)
   end
 
-  defp load(<<@stop>>, [stack]) do
+  defp load(<<@stack_global, rest::binary>>, [two, one | stack], memo) do
+    load(rest, [{:qualifier, one, two} | stack], memo)
+  end
+
+  defp load(<<@reduce, rest::binary>>, [arg, class | stack], memo) do
+    case class do
+      {:qualifier, "collections", "OrderedDict"} -> load(rest, [%{} | stack], memo)
+      _ -> load(rest, [{:reduce, class, arg} | stack], memo)
+    end
+  end
+
+  defp load(<<@bin_get, idx::little, rest::binary>>, stack, memo) do
+    n = length(memo)
+    el = Enum.at(memo, n - 1 - idx)
+    load(rest, [el | stack], memo)
+  end
+
+  defp load(<<@long_bin_get, idx::32-integer-little-unsigned, rest::binary>>, stack, memo) do
+    n = length(memo)
+    el = Enum.at(memo, n - 1 - idx)
+    load(rest, [el | stack], memo)
+  end
+
+  defp load(<<@new_false, rest::binary>>, stack, memo) do
+    load(rest, [false | stack], memo)
+  end
+
+  defp load(<<@new_true, rest::binary>>, stack, memo) do
+    load(rest, [true | stack], memo)
+  end
+
+  defp load(<<@new_none, rest::binary>>, stack, memo) do
+    load(rest, [nil | stack], memo)
+  end
+
+  defp load(<<@tuple, rest::binary>>, stack, memo) do
+    {leading, [:mark | rest_stack]} = Enum.split_while(stack, &(&1 != :mark))
+    tuple = List.to_tuple(leading)
+    load(rest, [tuple | rest_stack], memo)
+  end
+
+  defp load(<<@appends, rest::binary>>, stack, memo) do
+    {leading, [:mark, list_ | rest_stack]} = Enum.split_while(stack, &(&1 != :mark))
+    load(rest, [Enum.reverse(leading) ++ list_ | rest_stack], memo)
+  end
+
+  defp load(<<@append, rest::binary>>, [el, list_ | stack], memo) do
+    load(rest, [[el | list_] | stack], memo)
+  end
+
+  defp load(<<@build, rest::binary>>, [arg, object | stack], memo) do
+    load(rest, [{:build, object, arg} | stack], memo)
+  end
+
+  defp load(<<@empty_tuple, rest::binary>>, stack, memo) do
+    load(rest, [{} | stack], memo)
+  end
+
+  defp load(<<@empty_list, rest::binary>>, stack, memo) do
+    load(rest, [[] | stack], memo)
+  end
+
+  defp load(<<@new_object, rest::binary>>, [cls, args | stack], memo) do
+    load(rest, [{:new_object, cls, args} | stack], memo)
+  end
+
+  defp load(<<@empty_dict, rest::binary>>, stack, memo) do
+    load(rest, [%{} | stack], memo)
+  end
+
+  defp load(<<@empty_set, rest::binary>>, stack, memo) do
+    load(rest, [MapSet.new() | stack], memo)
+  end
+
+  defp load(
+         <<@binbytes, value::32-integer-little-unsigned, binary::size(value)-binary,
+           rest::binary>>,
+         stack,
+         memo
+       ) do
+    load(rest, [binary | stack], memo)
+  end
+
+  defp load(<<@set_items, rest::binary>>, stack, memo) do
+    {leading, [:mark, dict_ | rest_stack]} = Enum.split_while(stack, &(&1 != :mark))
+    chunked_leading = Enum.chunk_every(leading, 2)
+    new_dict = for [val, key] <- chunked_leading, into: %{}, do: {key, val}
+    final_dict = Map.merge(new_dict, dict_)
+    load(rest, [final_dict | rest_stack], memo)
+  end
+
+  defp load(<<@set_item, rest::binary>>, [value, key, dict_| stack], memo) do
+      new_dict = Map.put(dict_, key, value)
+      load(rest, [new_dict | stack], memo)
+  end
+
+  defp load(<<@stop>>, [stack], _) do
     {:ok, stack}
   end
 end
--- test/baking_soda_test.exs
+++ test/baking_soda_test.exs
@@ -6,44 +6,79 @@ defmodule BakingSodaTest do
   # binary representation of 6 (one byte int)
   @one_byte_int <<128, 4, 75, 6, 46>>
   test "one_byte_int" do
-    {:ok, res} = BakingSoda.load(@one_byte_int)
-    assert res == 6
+    assert {:ok, 6} = BakingSoda.load(@one_byte_int)
   end
 
   # binary representation of 7312
   @two_byte_int <<128, 4, 149, 4, 0, 0, 0, 0, 0, 0, 0, 77, 144, 28, 46>>
   test "two_byte_int" do
-    {:ok, res} = BakingSoda.load(@two_byte_int)
-    assert res == 7312
+    assert {:ok, 7312} = BakingSoda.load(@two_byte_int)
   end
 
   # binary representation of tuple (1,2)
   @tuple2 <<128, 4, 149, 7, 0, 0, 0, 0, 0, 0, 0, 75, 1, 75, 2, 134, 148, 46>>
   test "tuple" do
-    {:ok, res} = BakingSoda.load(@tuple2)
-    assert res == {1,2}
+    assert {:ok, {1,2}} = BakingSoda.load(@tuple2)
   end
 
   # binary representation of tuple (1,2,3)
   @tuple3 <<128, 4, 149, 9, 0, 0, 0, 0, 0, 0, 0, 75, 1, 75, 2, 75, 3, 135, 148, 46>>
   test "tuple3" do
-    {:ok, res} = BakingSoda.load(@tuple3)
-    assert res == {1,2,3}
+    assert {:ok, {1,2,3}} = BakingSoda.load(@tuple3)
   end
 
   # binary representation of nested tuple (1, 2, (3, 4))
   @nested_tuple <<128, 4, 149, 13, 0, 0, 0, 0, 0, 0, 0, 75, 1, 75, 2, 75, 3, 75, 4, 134, 148,
   135, 148, 46>>
   test "nested_tuple" do
-    {:ok, res} = BakingSoda.load(@nested_tuple)
-    assert res == {1,2,{3,4}}
+    assert {:ok, {1,2,{3,4}}} = BakingSoda.load(@nested_tuple)
   end
 
   # binary representation of double 1.0
   @float <<128, 4, 149, 10, 0, 0, 0, 0, 0, 0, 0, 71, 63, 240, 0, 0, 0, 0, 0, 0, 46>>
   test "float" do
-    {:ok, res} = BakingSoda.load(@float)
-    assert res == 1.0
+    assert {:ok, 1.0} = BakingSoda.load(@float)
+  end
+
+  # binary representation of string
+  @string <<128, 4, 149, 10, 0, 0, 0, 0, 0, 0, 0, 140, 6, 115, 116, 114, 105, 110, 103,
+  148, 46>>
+  test "string" do
+    assert {:ok, "string"} = BakingSoda.load(@string)
+  end
+
+  #binary representation of {"a" : 1}
+  @dict_with_one_pair <<128, 4, 149, 10, 0, 0, 0, 0, 0, 0, 0, 125, 148, 140, 1, 97, 148, 75, 1, 115,
+  46>>
+  test "dict_with_one_pair" do
+    assert {:ok, %{"a" => 1}} = BakingSoda.load(@dict_with_one_pair)
+  end
+
+  #binary representation of {"a":1, "b":2, "c":3}
+  @dict <<128, 4, 149, 23, 0, 0, 0, 0, 0, 0, 0, 125, 148, 40, 140, 1, 97, 148, 75, 1,
+  140, 1, 98, 148, 75, 2, 140, 1, 99, 148, 75, 3, 117, 46>>
+  test "dict" do
+    assert {:ok, %{"a" => 1, "b" => 2, "c" => 3}} = BakingSoda.load(@dict)
+  end
+
+  #binary representation of [1,2,3,"a"]
+  @list <<128, 4, 149, 15, 0, 0, 0, 0, 0, 0, 0, 93, 148, 40, 75, 1, 75, 2, 75, 3, 140,
+  1, 97, 148, 101, 46>>
+  test "list" do
+    assert {:ok, [1,2,3,"a"]} = BakingSoda.load(@list)
+  end
+
+  #binary representation of [1]
+  @list_one <<128, 4, 149, 6, 0, 0, 0, 0, 0, 0, 0, 93, 148, 75, 1, 97, 46>>
+  test "list_one" do
+    assert {:ok, [1]} = BakingSoda.load(@list_one)
+  end
+
+  #binary representation of [1,2,[3, 4,[5]]]
+  @nested_list <<128, 4, 149, 22, 0, 0, 0, 0, 0, 0, 0, 93, 148, 40, 75, 1, 75, 2, 93, 148, 40,
+  75, 3, 75, 4, 93, 148, 75, 5, 97, 101, 101, 46>>
+  test "nested_list" do
+    assert {:ok, [1,2,[3, 4,[5]]]} = BakingSoda.load(@nested_list)
   end
 
 
